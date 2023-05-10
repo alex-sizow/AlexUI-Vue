26c1815855eceefb0f00f58f7be5512e90e22dd3
Alex <alex_sizow@outlook.com>
2023-05-10 19:28:11 +0300

    Input init

--- src/views/Input.vue
+++ src/views/Input.vue
@@ -1,7 +1,39 @@
+<script setup>
+import useVuelidate from '@vuelidate/core';
+import { minLength } from '@vuelidate/validators';
+import { computed, ref } from 'vue';
+
+import Input from '@/components/Input.vue';
+
+const nameField = ref('');
+
+const rules = computed(() => ({
+	nameField: {
+		minLength: minLength(3),
+	},
+}));
+
+const v = useVuelidate(rules, { nameField });
+</script>
+
 <template>
-	<div>sdfdf</div>
+	<h1 class="header-1">Inputs</h1>
+	<h2 class="header-2">Input</h2>
+
+	<form>
+		<Input
+			label="Name"
+			placeholder="Hello"
+			:value="nameField"
+			v-model:value="v.nameField.$model" />
+	</form>
 </template>
 
-<script setup></script>
-
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h2 {
+	margin-top: 20px;
+}
+form {
+	margin-top: 20px;
+}
+</style>
